37445594d13a0e0a3604183f54c5359831971663
Chaya Carey <chaya.carey@dbtlabs.com>
2025-11-10 10:02:22 -0800

    Identify and parse package dependency files

---
+++ dbt_autofix/packages/package_upgrade_versions.py
@@ -0,0 +1,150 @@
+from collections import defaultdict
+from dataclasses import dataclass, field
+from pathlib import Path
+from pprint import pprint
+from typing import Any, Optional, Union
+from semver.version import Version
+import yaml
+import yamllint
+import yamllint.linter
+import yamllint.config
+from rich.console import Console
+from dbt_autofix.refactors.yml import DbtYAML, read_file
+
+console = Console()
+
+config = """
+rules:
+  key-duplicates: enable
+"""
+
+yaml_config = yamllint.config.YamlLintConfig(config)
+
+VALID_PACKAGE_YML_NAMES: set[str] = set(['packages.yml', 'dependencies.yml'])
+
+@dataclass
+class DbtPackage:
+    package_dict: Optional[dict[str, Union[str,list[str]]]]
+    package_id: Optional[str]
+    package_name: Optional[str]
+    package_version_str: Optional[str]
+    package_version: Optional[Version]
+    current_project_package_version_str: Optional[str]
+    current_project_package_version: Optional[Version]
+    current_project_package_version_range_str: Optional[list[str]]
+    current_project_package_version_range: Optional[list[Version]]
+    require_dbt_version: Optional[list[str]]
+    min_upgradeable_version: Optional[str]
+    max_upgradeable_version: Optional[str]
+    lowest_fusion_compatible_version: Optional[str]
+    fusion_compatible_versions: Optional[list[Version]]
+    git_url: Optional[str]
+    opt_in_prerelease: bool = False
+
+    def __post_init__(self):
+        pass
+
+    def parse_current_package_version(self):
+        pass
+
+
+@dataclass
+class DbtPackageFile:
+    file_path: Optional[Path]
+    yml_str: str = ""
+    yml_dict: list[dict[str, Union[str, list[str]]]] = field(default_factory=list)
+    package_dependencies: dict[str, DbtPackage] = field(default_factory=dict)
+
+    def parse_file_path_to_string(self):
+        if not self.file_path:
+            console.log("No file path set")
+            return
+        try:
+            self.yml_str = self.file_path.read_text()
+            console.log(f"parsed yaml string: {self.yml_str}")
+        except:
+            console.log(f"Error when parsing package file {self.file_path}")
+    
+    def parse_yml_string_to_dict(self):
+        if not self.yml_str:
+            console.log("No YML string found, use parse_file_path_to_string first")
+        try:
+            parsed_package_file = DbtYAML().load(self.yml_str) or {}
+        except:
+            console.log(f"Error when parsing package file {self.file_path}")
+            return
+        if parsed_package_file == {}:
+            console.log("No content parsed")
+            return
+        if "packages" not in parsed_package_file:
+            console.log("File does not contain packages key")
+            return
+        for package in parsed_package_file["packages"]:
+            print(f"package: {package}")
+            self.yml_dict.append(package)
+        else:
+            console.log(f"Package file {self.file_path} could not be parsed")
+        console.log(pprint(self.yml_dict))
+
+    def parse_package_dependencies(self):
+        for k, v in self.yml_dict:
+            console.log(f"k: {k}, v: {v}")
+
+    def add_package_dependency(self, package_name: str, package: DbtPackage):
+        if package_name in self.package_dependencies:
+            console.log(f"{package_name} is already in the dependencies")
+        else:
+            self.package_dependencies[package_name] = package
+
+
+def find_package_paths(
+    root_dir: Path,
+) -> list[Path]:
+    packages_path = yaml.safe_load((root_dir / "dbt_project.yml").read_text()).get(
+        "packages-install-path", "dbt_packages"
+    )
+
+    yml_files_packages = set((root_dir / packages_path).glob("**/*.yml")).union(
+        set((root_dir / packages_path).glob("**/*.yaml"))
+    )
+
+    # this is a hack to avoid checking integration_tests. it won't work everywhere but it's good enough for now
+    yml_files_packages_integration_tests = set((root_dir / packages_path).glob("**/integration_tests/**/*.yml")).union(
+        set((root_dir / packages_path).glob("**/integration_tests/**/*.yaml"))
+    )
+    yml_files_packages_not_integration_tests = yml_files_packages - yml_files_packages_integration_tests
+
+    return [Path(str(path)) for path in yml_files_packages_not_integration_tests]
+
+
+def find_package_yml_files(
+    root_dir: Path,
+) -> list[Path]:
+    yml_files = set(root_dir.glob("**/*.yml")).union(set(root_dir.glob("**/*.yaml")))
+
+    package_yml_files = []
+
+
+    for yml_file in yml_files:
+        if yml_file.name in VALID_PACKAGE_YML_NAMES:
+            package_yml_files.append(yml_file)
+    
+    if len(package_yml_files) == 0:
+        console.log("No package YML files found")
+    return package_yml_files
+
+
+def parse_package_files(package_file_paths: list[Path]) -> list[DbtPackageFile]:
+    if package_file_paths == []:
+        return []
+    
+    package_files: list[DbtPackageFile] = []
+    for path in package_file_paths:
+        console.log(f"package path: {path}")
+        package_file = DbtPackageFile(path)
+        package_file.parse_file_path_to_string()
+        package_file.parse_yml_string_to_dict()
+        package_file.parse_package_dependencies()
+        package_files.append(package_file)
+    return package_files
+
---
+++ tests/unit_tests/test_package_upgrade.py
@@ -0,0 +1,160 @@
+from pprint import pprint
+import tempfile
+from pathlib import Path
+from dbt_autofix.packages.package_upgrade_versions import find_package_yml_files, parse_package_files, DbtPackageFile
+
+import pytest
+
+@pytest.fixture
+def temp_project_dir():
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        project_dir = Path(tmpdirname)
+
+        # Create dbt_project.yml
+        project_dir.joinpath("dbt_project.yml").write_text("""
+packages-install-path: dbt_packages
+""")
+
+        # Create project YAML files
+        models_dir = project_dir / "models"
+        models_dir.mkdir(parents=True, exist_ok=True)
+        models_dir.joinpath("schema.yml").write_text("""
+version: 2
+
+models:
+  - name: model1
+    description: "First model"
+    name: other_name
+  - name: model2
+    description: "Second model"
+""")
+        
+        # Create package YAML file
+        project_dir.joinpath("packages.yml").write_text("""
+packages:
+  - package: dbt-labs/dbt_external_tables
+    version: [">=0.8.0", "<0.9.0"]
+  
+  - package: dbt-labs/dbt_utils
+    version: [">=0.9.0", "<1.0.0"]
+
+  - package: dbt-labs/codegen
+    version: [">=0.8.0", "<0.9.0"]
+
+  - package: dbt-labs/audit_helper
+    version: [">=0.6.0", "<0.7.0"]
+
+  - package: metaplane/dbt_expectations
+    version: [">=0.10.8", "<1.0.0"]
+
+  - git: "https://github.com/PrivateGitRepoPackage/gmi_common_dbt_utils.git"
+    revision: main # use a branch or a tag name
+""")
+        
+        # Create package lock file
+        project_dir.joinpath("package-lock.yml").write_text("""
+packages:
+  - name: dbt_external_tables
+    package: dbt-labs/dbt_external_tables
+    version: 0.8.7
+  - name: dbt_utils
+    package: dbt-labs/dbt_utils
+    version: 0.9.6
+  - name: codegen
+    package: dbt-labs/codegen
+    version: 0.8.1
+  - name: audit_helper
+    package: dbt-labs/audit_helper
+    version: 0.6.0
+  - name: dbt_expectations
+    package: metaplane/dbt_expectations
+    version: 0.10.9
+  - git: https://github.com/PrivateGitRepoPackage/gmi_common_dbt_utils.git
+    name: gmi_common_dbt_utils
+    revision: 067b588343e9c19dc8593b6b3cb06cc5b47822e1
+  - name: dbt_date
+    package: godatadriven/dbt_date
+    version: 0.16.1
+sha1_hash: f10149243aadecf4a289805e5892180d9fc50142
+
+""")
+
+        # Create package YAML files without duplicates
+        package_dir = project_dir / "dbt_packages" / "test_package"
+        package_dir.mkdir(parents=True, exist_ok=True)
+        package_models_dir = package_dir / "models"
+        package_models_dir.mkdir(parents=True, exist_ok=True)
+        package_models_dir.joinpath("schema.yml").write_text("""
+version: 2
+
+models:
+  - name: package_model1
+    description: "First package model"
+  - name: package_model2
+    name: package_model3
+    description: "Duplicate package model"
+""")
+
+        # Create integration test files (should be ignored)
+        integration_dir = package_dir / "integration_tests"
+        integration_dir.mkdir(parents=True, exist_ok=True)
+        integration_dir.joinpath("schema.yml").write_text("""
+version: 2
+
+models:
+  - name: integration_model1
+    description: "First integration model"
+    description: "First integration model duplicate"
+  - name: integration_model2
+    description: "Duplicate integration model"
+    
+""")
+
+        yield project_dir
+
+
+def test_find_package_files(temp_project_dir: Path):
+    package_files = find_package_yml_files(temp_project_dir)
+    assert len(package_files) == 1
+    assert package_files[0].name == "packages.yml"
+    assert package_files[0] == Path(f"{temp_project_dir}/packages.yml")
+
+
+def test_parse_file_path_to_string(temp_project_dir: Path):
+    package_files = find_package_yml_files(temp_project_dir)
+    package_file = DbtPackageFile(file_path=package_files[0])
+    package_file.parse_file_path_to_string()
+    assert package_file.file_path is not None
+    assert package_file.yml_str != ""
+    assert len(package_file.yml_str) > 0
+    assert package_file.yml_str == """
+packages:
+  - package: dbt-labs/dbt_external_tables
+    version: [">=0.8.0", "<0.9.0"]
+  
+  - package: dbt-labs/dbt_utils
+    version: [">=0.9.0", "<1.0.0"]
+
+  - package: dbt-labs/codegen
+    version: [">=0.8.0", "<0.9.0"]
+
+  - package: dbt-labs/audit_helper
+    version: [">=0.6.0", "<0.7.0"]
+
+  - package: metaplane/dbt_expectations
+    version: [">=0.10.8", "<1.0.0"]
+
+  - git: "https://github.com/PrivateGitRepoPackage/gmi_common_dbt_utils.git"
+    revision: main # use a branch or a tag name
+"""
+
+
+def test_parse_package_string_to_dict(temp_project_dir: Path):
+    package_files = find_package_yml_files(temp_project_dir)
+    package_file = DbtPackageFile(file_path=package_files[0])
+    package_file.parse_file_path_to_string()
+    package_file.parse_yml_string_to_dict()
+    pprint(package_file.yml_dict)
+
+    assert len(package_files) == 1
+
