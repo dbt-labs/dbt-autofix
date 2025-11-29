e2fc2f18a5938d496be3060fcf76e5fb38237f55
Chaya Carey <chaya.carey@dbtlabs.com>
2025-11-28 17:30:33 -0800

    add unit test for renaming package from file

--- tests/unit_tests/package_upgrades/test_dbt_package_text_file.py
+++ tests/unit_tests/package_upgrades/test_dbt_package_text_file.py
@@ -1,42 +1,14 @@
-from pprint import pprint
 import tempfile
 from pathlib import Path
-from dbt_autofix.packages.dbt_package_file import (
-    DbtPackageFile,
-    load_yaml_from_packages_yml,
-    parse_package_dependencies_from_packages_yml,
-    parse_package_dependencies_from_yml,
-    find_package_yml_files,
-)
-
 import pytest
-
-from dbt_autofix.packages.dbt_package_text_file import DbtPackageTextFileLine
+from dbt_autofix.packages.dbt_package_file import find_package_yml_files
+from dbt_autofix.packages.dbt_package_text_file import DbtPackageTextFile, DbtPackageTextFileLine
 
 
 @pytest.fixture
 def temp_project_dir_with_packages_yml():
     with tempfile.TemporaryDirectory() as tmpdirname:
         project_dir = Path(tmpdirname)
-
-        # Create dbt_project.yml
-        project_dir.joinpath("dbt_project.yml").write_text("""
-packages-install-path: dbt_packages
-""")
-
-        # Create project YAML files
-        models_dir = project_dir / "models"
-        models_dir.mkdir(parents=True, exist_ok=True)
-        models_dir.joinpath("schema.yml").write_text("""
-version: 2
-
-models:
-  - name: model1
-    description: "First model"
-    name: other_name
-  - name: model2
-    description: "Second model"
-""")
 
         # Create package YAML file
         project_dir.joinpath("packages.yml").write_text("""
@@ -58,66 +30,9 @@ packages:
 
   - git: "https://github.com/PrivateGitRepoPackage/gmi_common_dbt_utils.git"
     revision: main # use a branch or a tag name
-""")
-
-        # Create package lock file
-        project_dir.joinpath("package-lock.yml").write_text("""
-packages:
-  - name: dbt_external_tables
-    package: dbt-labs/dbt_external_tables
-    version: 0.8.7
-  - name: dbt_utils
-    package: dbt-labs/dbt_utils
-    version: 0.9.6
-  - name: codegen
-    package: dbt-labs/codegen
-    version: 0.8.1
-  - name: audit_helper
-    package: dbt-labs/audit_helper
-    version: 0.6.0
-  - name: dbt_expectations
-    package: metaplane/dbt_expectations
-    version: 0.10.9
-  - version: 0.10.9
-  - git: https://github.com/PrivateGitRepoPackage/gmi_common_dbt_utils.git
-    name: gmi_common_dbt_utils
-    revision: 067b588343e9c19dc8593b6b3cb06cc5b47822e1
-  - name: dbt_date
-    package: godatadriven/dbt_date
-    version: 0.16.1
-sha1_hash: f10149243aadecf4a289805e5892180d9fc50142
-
-""")
-
-        # Create package YAML files without duplicates
-        package_dir = project_dir / "dbt_packages" / "test_package"
-        package_dir.mkdir(parents=True, exist_ok=True)
-        package_models_dir = package_dir / "models"
-        package_models_dir.mkdir(parents=True, exist_ok=True)
-        package_models_dir.joinpath("schema.yml").write_text("""
-version: 2
-
-models:
-  - name: package_model1
-    description: "First package model"
-  - name: package_model2
-    name: package_model3
-    description: "Duplicate package model"
-""")
-
-        # Create integration test files (should be ignored)
-        integration_dir = package_dir / "integration_tests"
-        integration_dir.mkdir(parents=True, exist_ok=True)
-        integration_dir.joinpath("schema.yml").write_text("""
-version: 2
-
-models:
-  - name: integration_model1
-    description: "First integration model"
-    description: "First integration model duplicate"
-  - name: integration_model2
-    description: "Duplicate integration model"
-    
+                                                        
+  - version: "0.10.1"
+    package: calogica/dbt_date
 """)
 
         yield project_dir
@@ -256,3 +171,9 @@ def test_replace_package_name_in_line(input_str, expected_match):
     assert replaced
     assert len(file_line.line) == len(expected_match)
     assert file_line.line == expected_match
+
+
+def test_rename_package(temp_project_dir_with_packages_yml):
+    package_files = find_package_yml_files(temp_project_dir_with_packages_yml)
+    file = DbtPackageTextFile(package_files[0])
+    file.update_config_file({"calogica/dbt_date": "0.17.0"}, dry_run=True, print_to_console=True)
