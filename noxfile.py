784a31c97d56678c1da1165076ec4022aefe7b46
MichelleArk <michelle.ark@dbtlabs.com>
2025-12-19 17:12:05 -0500

    linting

--- noxfile.py
+++ noxfile.py
@@ -3,7 +3,7 @@ import nox
 nox.options.default_venv_backend = "uv"
 
 
-@nox.session(python=[ "3.10", "3.11", "3.12", "3.13"], venv_backend="uv")
+@nox.session(python=["3.10", "3.11", "3.12", "3.13"], venv_backend="uv")
 def run_cli(session):
     """Make sure the CLI runs correctly"""
     session.run_install(
