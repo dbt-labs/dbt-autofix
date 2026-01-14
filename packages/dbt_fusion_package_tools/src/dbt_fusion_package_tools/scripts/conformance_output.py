eee2d34f865dac3cf6f65b270dee652c01ced0db
Chaya Carey <chaya.carey@dbtlabs.com>
2026-01-14 15:09:17 -0800

    clean up CSV output script

--- packages/dbt_fusion_package_tools/src/dbt_fusion_package_tools/scripts/conformance_output.py
+++ packages/dbt_fusion_package_tools/src/dbt_fusion_package_tools/scripts/conformance_output.py
@@ -1,13 +1,9 @@
-from collections import defaultdict
+from collections import Counter, defaultdict
 import csv
 import json
 from pathlib import Path
 from typing import Any
-
-from dbt_fusion_package_tools.fusion_version_compatibility_output import (
-    FUSION_VERSION_COMPATIBILITY_OUTPUT,
-)
-from dbt_fusion_package_tools.compatibility import FusionConformanceResult
+from datetime import datetime
 
 
 def reload_output_from_file(
@@ -18,36 +14,57 @@ def reload_output_from_file(
 
 
 def main():
-    file_path: Path = (
-        Path.cwd() / "src" / "dbt_fusion_package_tools" / "scripts" / "output" / "v89_conformance_output_20251222.json"
-    )
-    fusion_version = "89"
-    conformance_output = reload_output_from_file(file_path)
+    output_path: Path = Path.cwd() / "src" / "dbt_fusion_package_tools" / "scripts" / "output"
+    conformance_output_file_name: str = "v92_conformance_output_20260112.json"
+    fusion_version = "92"
+    run_date: str = datetime.today().strftime("%Y%m%d")
+    package_summary_file_name: str = f"package_conformance_summary_v{fusion_version}_{run_date}.csv"
+    package_errors_file_name: str = f"package_conformance_all_errors_v{fusion_version}_{run_date}.csv"
+    conformance_output = reload_output_from_file(output_path / conformance_output_file_name)
     package_summary = []
-    unique_error_codes: set[str] = set()
+    unique_error_codes: Counter[str] = Counter()
+    unique_warning_codes: Counter[str] = Counter()
+    all_errors = []
     for package_name in conformance_output:
         package: dict[str, Any] = conformance_output[package_name]
         for version in package:
             package_version: dict[str, Any] = package[version]
-            parse_errors = []
-            parse_warnings = []
+            parse_errors: set[str] = set()
+            parse_warnings: set[str] = set()
             parse_compatibility_result = package_version.get("parse_compatibility_result") or {}
             if parse_compatibility_result.get("errors"):
                 for error in parse_compatibility_result["errors"]:
-                    parse_errors.append(error["error_code"])
-                    unique_error_codes.add(str(error["error_code"]))
+                    parse_errors.add(str(error["error_code"]))
+                    unique_error_codes[str(error["error_code"])] += 1
+                    all_errors.append(
+                        {
+                            "package_name": package_name,
+                            "package_version": version,
+                            "manually_verified_compatible": package[version]["manually_verified_compatible"],
+                            "manually_verified_incompatible": package[version]["manually_verified_incompatible"],
+                            "parse_compatible": package[version]["parse_compatible"],
+                            "parse_error_code": str(error["error_code"]),
+                            "parse_error": error["body"],
+                            "parse_exit_code": parse_compatibility_result.get("parse_exit_code"),
+                            "parse_total_errors": parse_compatibility_result.get("total_errors"),
+                            "parse_total_warnings": parse_compatibility_result.get("total_warnings"),
+                            "require_dbt_version_compatible": package[version]["require_dbt_version_compatible"],
+                            "require_dbt_version_defined": package[version]["require_dbt_version_defined"],
+                            "fusion_version": f"v{fusion_version}",
+                        }
+                    )
             if parse_compatibility_result.get("warnings"):
                 for warning in parse_compatibility_result["warnings"]:
-                    parse_errors.append(warning["error_code"])
-                    unique_error_codes.add(str(warning["error_code"]))
+                    parse_warnings.add(str(warning["error_code"]))
+                    unique_warning_codes[str(error["error_code"])] += 1
             package_summary.append(
                 {
                     "package_name": package_name,
                     "package_version": version,
                     "manually_verified_compatible": package[version]["manually_verified_compatible"],
                     "manually_verified_incompatible": package[version]["manually_verified_incompatible"],
-                    "parse_errors": parse_errors,
-                    "parse_warnings": parse_warnings,
+                    "parse_errors": ",".join(sorted(parse_errors)) if len(parse_errors) > 0 else "",
+                    "parse_warnings": ",".join(sorted(parse_warnings)) if len(parse_warnings) > 0 else "",
                     "parse_exit_code": parse_compatibility_result.get("parse_exit_code"),
                     "parse_total_errors": parse_compatibility_result.get("total_errors"),
                     "parse_total_warnings": parse_compatibility_result.get("total_warnings"),
@@ -57,23 +74,20 @@ def main():
                     "fusion_version": f"v{fusion_version}",
                 }
             )
-    print(f"unique error codes:")
-    for error_code in unique_error_codes:
-        print(error_code)
-    field_names = [field for field in package_summary[0]]
-    output_file = (
-        Path.cwd()
-        / "src"
-        / "dbt_fusion_package_tools"
-        / "scripts"
-        / "output"
-        / f"package_conformance_v{fusion_version}.csv"
-    )
-    with open(output_file, mode="w") as file:
-        writer = csv.DictWriter(file, fieldnames=field_names)
+    print(f"unique error codes: {unique_error_codes}")
+    print(f"unique warning codes: {unique_warning_codes}")
+
+    with open(output_path / package_summary_file_name, mode="w") as file:
+        writer = csv.DictWriter(file, fieldnames=[field for field in package_summary[0]])
         writer.writeheader()
         writer.writerows(package_summary)
-    print(f"Wrote {len(package_summary)} rows to conformance_output_v{fusion_version}.csv")
+    print(f"Wrote {len(package_summary)} rows to {output_path / package_summary_file_name}")
+
+    with open(output_path / package_errors_file_name, mode="w") as file:
+        writer = csv.DictWriter(file, fieldnames=[field for field in all_errors[0]])
+        writer.writeheader()
+        writer.writerows(all_errors)
+    print(f"Wrote {len(all_errors)} rows to {output_path / package_errors_file_name}")
 
 
 if __name__ == "__main__":
