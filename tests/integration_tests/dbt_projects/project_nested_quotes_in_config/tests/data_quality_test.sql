5fb24c12fd83b3d91b067c4cc36c59330f4b6641
David Harting <hartingdavid@icloud.com>
2026-03-11 16:09:15 -0400

    Add config example with 'this "type" of nesting'

--- tests/integration_tests/dbt_projects/project_nested_quotes_in_config/tests/data_quality_test.sql
+++ tests/integration_tests/dbt_projects/project_nested_quotes_in_config/tests/data_quality_test.sql
@@ -1,7 +1,8 @@
 {{ config(
     type_of_dq_test = 'base',
     severity = 'warn',
-    warn_if = '== "never true"'
+    warn_if = '== "never true"',
+    error_if = "!= 'always true'"
 ) }}
 
 select 1 as id
