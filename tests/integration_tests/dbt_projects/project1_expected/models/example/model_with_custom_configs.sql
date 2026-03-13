9a02fb53458531cba517d6743cf607c67d824948
Diego Fernandez <diego.fernandez@dbtlabs.com>
2026-03-13 15:03:50 -0600

    Also save expected output during GOLDIE_UPDATE=1
    The console output of running autofix is important, but the actual
    changes it makes to the project are arguably more important.
    
    This makes it much easier to see the output of re-running autofix on the
    project and ensure the changes are expected.

--- tests/integration_tests/dbt_projects/project1_expected/models/example/model_with_custom_configs.sql
+++ tests/integration_tests/dbt_projects/project1_expected/models/example/model_with_custom_configs.sql
@@ -3,6 +3,7 @@
     meta={'existing_meta_config': 'existing_meta_config', 'custom_config': 'custom_config', 'custom_config_int': 2, 'custom_config_list': ['a', 'b', 'c'], 'custom_config_dict': {'a': 1, 'b': 2, 'c': 3}}
 ) }}
 
+
 {{ config.meta_get('custom_config') }}
 
 {{ config.meta_get('custom_config', 'default_value') }}
